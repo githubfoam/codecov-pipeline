c3945279c1278fb68fe415d44486a3b7095e6a32
githubfoam <lequestionis@gmail.com>
2020-11-21 14:50:28 +0300

    init

---
+++ ruby-pipeline/app1/test/helper.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'rubygems'
+require 'bundler/setup'
+
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/test/'
+end
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov if ENV['CI'] == 'true'
+
+require 'minitest/autorun'
+require 'mocha/minitest'
+require 'webmock/minitest'
+
+require 'minitest/ci'
+Minitest::Ci.report_dir = '.' if ENV['CIRCLECI']
