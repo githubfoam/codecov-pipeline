5350541f306f5f928bd1993b52a63f7f8aa12177
johnny bravo <lequestionis@gmail.com>
2021-12-11 12:07:46 +0300

    upper

---
+++ app2/build.gradle.kts
@@ -0,0 +1,9 @@
+tasks.register("upper") {
+    doLast {
+        val someString = "mY_nAmE"
+        println("Original: $someString")
+        println("Upper case: ${someString.toUpperCase()}")
+    }
+}
+
+
